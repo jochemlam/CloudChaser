e65686a46a50e5cde060c0cea2fa5eb3194bbeb3
j.lammertsma <j.lammertsma@azerion.com>
2021-03-03 09:56:59 +0100

    import tmp and created selection screens

---
+++ Assets/_Game/Scripts/UI/MenuInteractions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInteractions : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneSwitcher.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Game/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public delegate string SceneSwitch();
+    public static event SceneSwitch OnSceneSwitch;
+    public static void LoadScene(string newScene)
+    {
+        SceneManager.LoadScene(newScene);
+
+        if (OnSceneSwitch != null)
+            OnSceneSwitch();
+    }
+}
