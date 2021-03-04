9cb65c2191bb0d329483ab857f3c4c5adf10d1a2
j.lammertsma <j.lammertsma@azerion.com>
2021-03-04 10:05:59 +0100

    added pause prefab, created new scenes and improved ui interaction

--- Assets/_Game/Scripts/UI/MenuInteractions.cs
+++ Assets/_Game/Scripts/UI/MenuInteractions.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class MenuInteractions : MonoBehaviour
 {
+    private bool gameIsPaused = false;
+    public void ToggleGamePause()
+    {
+        gameIsPaused = !gameIsPaused;
+
+        if (gameIsPaused)
+        {
+            ToggleMenuItem(this.gameObject, true);
+        }
+        else
+        {
+            ToggleMenuItem(this.gameObject, false);
+        }
+    }
+
+    public void GoToMenu()
+    {
+        SceneSwitcher.LoadScene("MainMenu");
+    }
+
+    private void ToggleMenuItem(GameObject gameObject, bool state)
+    {
+        gameObject.SetActive(state);
+    }
     public void StartGame()
     {
         SceneSwitcher.LoadScene("Main");
--- Assets/_Game/Scripts/UI/SceneSwitcher.cs
+++ Assets/_Game/Scripts/UI/SceneSwitcher.cs
@@ -7,9 +7,9 @@ public class SceneSwitcher : MonoBehaviour
 {
     public delegate string SceneSwitch();
     public static event SceneSwitch OnSceneSwitch;
-    public static void LoadScene(string newScene)
+    public static void LoadScene(string scene)
     {
-        SceneManager.LoadScene(newScene);
+        SceneManager.LoadScene(scene);
 
         if (OnSceneSwitch != null)
             OnSceneSwitch();
