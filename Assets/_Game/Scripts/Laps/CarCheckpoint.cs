31782f5173b3cb65796718a03b03ff15c6063b35
j.lammertsma <j.lammertsma@azerion.com>
2021-03-05 16:18:13 +0100

    added laps system and updated gitignore

---
+++ Assets/_Game/Scripts/Laps/CarCheckpoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCheckpoint : MonoBehaviour
+{
+    public Transform[] checkPointArray;
+    public static int currentCheckpoint = 0;
+    public static int currentLap = 0;
+}
