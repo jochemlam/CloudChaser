42c4ed02e91ab7fea7ef775a70494868ff5eeb05
j.lammertsma <j.lammertsma@azerion.com>
2021-03-10 12:00:07 +0100

    set max laps now

--- Assets/_Game/Scripts/Laps/CarCheckpoint.cs
+++ Assets/_Game/Scripts/Laps/CarCheckpoint.cs
@@ -7,4 +7,23 @@ public class CarCheckpoint : MonoBehaviour
     public Transform[] checkPointArray;
     public static int currentCheckpoint = 0;
     public static int currentLap = 0;
+
+    [SerializeField]
+    private int totalLapCount = 3;
+
+    public delegate void OnLapsFinishedDelegate();
+    public static OnLapsFinishedDelegate onRaceFinished;
+
+    private void Update()
+    {
+        LapsFinished();
+    }
+
+    private void LapsFinished()
+    {
+        if (currentLap == totalLapCount)
+        {
+            onRaceFinished();
+        }
+    }
 }
